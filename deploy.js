56b5db970d3f7b42c556d3c1bbb1a2f6e0c1830d
Raymond Hsu <raymondhsu@duck.com>
2024-06-25 01:13:43 +0800

    update some usage
    commands deploying files seems to be outdated on api usage

--- deploy.js
+++ deploy.js
@@ -1,47 +1,57 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord.js");
+const { REST, Routes } = require("discord.js");
 require("dotenv").config();
 
 const commands = [];
 const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
 
 const clientId = process.env.CLIENTID;
 const guildId = process.env.GUILDID;
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	commands.push(command.data.toJSON());
+  const command = require(filePath);
+  if ("data" in command && "execute" in command) {
+    commands.push(command.data.toJSON());
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
 }
 
-const rest = new REST({ version: "10" }).setToken(process.env.DISCORD);
+// This file will deploy both guild and global commands by default.
 
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(process.env.DISCORD);
+
+// Deploy guild commands 
 (async () => {
   try {
-    console.log("Started reloading guild application (/) commands.");
+    console.log(`Started reloading ${commands.length} guild application (/) commands.`);
 
-    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-      body: commands,
-    });
+    const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
 
-    console.log("Successfully reloaded guild application (/) commands.");
+    console.log(`Successfully reloaded ${data.length} guild application (/) commands.`);
   } catch (error) {
     console.error(error);
   }
 })();
 
+// Deploy global commands
 (async () => {
   try {
-    console.log("Started reloading global application (/) commands.");
+    console.log(`Started reloading ${commands.length} global application (/) commands.`);
 
-    await rest.put(Routes.applicationCommands(clientId), { body: commands });
+    const data = await rest.put(
+			Routes.applicationCommands(clientId, guildId),
+			{ body: commands },
+		);
 
-    console.log("Successfully reloaded global application (/) commands.");
+    console.log(`Successfully reloaded ${data.length} global application (/) commands.`);
   } catch (error) {
     console.error(error);
   }
--- undeploy.js
+++ undeploy.js
@@ -1,12 +1,9 @@
 const fs = require("node:fs");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+const { REST, Routes } = require("discord.js");
 require("dotenv").config();
 
 const commands = [];
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
 
 const clientId = process.env.CLIENTID;
 const guildId = process.env.GUILDID;
@@ -16,7 +13,7 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: "9" }).setToken(process.env.DISCORD);
+const rest = new REST().setToken(process.env.DISCORD);
 
 // This will only remove guild commands, since guild commands can create duplicated commands in the specified guild.
 
@@ -24,19 +21,14 @@ const rest = new REST({ version: "9" }).setToken(process.env.DISCORD);
   try {
     console.log("Started removing guild application (/) commands.");
 
-    await rest
-      .get(Routes.applicationGuildCommands(clientId, guildId))
-      .then((data) => {
-        const promises = [];
-        for (const command of data) {
-          const deleteUrl = `${Routes.applicationGuildCommands(
-            clientId,
-            guildId
-          )}/${command.id}`;
-          promises.push(rest.delete(deleteUrl));
-        }
-        return Promise.all(promises);
-      });
+    await rest.get(Routes.applicationGuildCommands(clientId, guildId)).then((data) => {
+      const promises = [];
+      for (const command of data) {
+        const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
+        promises.push(rest.delete(deleteUrl));
+      }
+      return Promise.all(promises);
+    });
 
     console.log("Successfully removed guild application (/) commands.");
   } catch (error) {
